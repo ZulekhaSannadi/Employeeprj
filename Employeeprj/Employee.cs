78aa10e1011b9003e5fc04f01de42f2def250c14
zsanadi <zsanadi@BMINP5717-2304>
2023-04-22 20:59:20 +0530

    Add project files.

---
+++ Employeeprj/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeeprj
+{
+    internal class Employee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Design{ get; set; }
+       public string Department { get; set; }
+        public double Salary { get; set; }
+
+
+        public void InputDetails()
+        {
+            Console.Write("Enter employee ID: ");
+            Id= Console.ReadLine();
+
+            Console.Write("Enter employee name: ");
+            Name = Console.ReadLine();
+
+            Console.Write("Enter employee Designation: ");
+            Design = Console.ReadLine();
+
+            Console.Write("Enter employee department: ");
+            Department = Console.ReadLine();
+
+            Console.Write("Enter employee salary: ");
+            Salary = double.Parse(Console.ReadLine());
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("\nEmployee Details:");
+            Console.WriteLine("Id: " + Id);
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Designation: " + Design );
+            Console.WriteLine("Department: " + Department);
+            Console.WriteLine("Salary: " + Salary);
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++");
+
+        }
+    }
+}
+
---
+++ Employeeprj/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeeprj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            int choice;
+
+            do
+            {
+                Console.Write("\nEnter your choice: ");
+                Console.WriteLine("\n1. Input employee details");
+                Console.WriteLine("2. Display employee details");
+                Console.WriteLine("3. Delete employee");
+                Console.WriteLine("4. Exit");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Employee emp = new Employee();
+                        emp.InputDetails();
+                        employees.Add(emp);
+                        break;
+                    case 2:
+                        foreach (Employee e in employees)
+                        {
+                            e.DisplayDetails();
+                        }
+                        break;
+                    case 3:
+                        Console.Write("Enter employee name to delete: ");
+                        string name = Console.ReadLine();
+                        employees.RemoveAll(e => e.Name == name);
+                        Console.WriteLine("successfully deleted");
+                        break;
+                    case 4:
+                        Console.WriteLine("Exiting program...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            } while (choice != 4);
+
+            Console.ReadKey();
+        }
+    }
+
+}
